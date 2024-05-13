8685dcf3664ccec9a92d7876302ee1dcd64351cb
Nicole Linnarz <mikaelafs17@gmail.com>
2024-05-13 09:46:41 -0600

    env keys

--- exercise5-13.js
+++ exercise5-13.js
@@ -3,11 +3,11 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
 
-app.use(express.static("5-13-folder"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/students?/:id", (req, res) => {
-  res.send(`arrived ${req.params.id}\n`);
+  res.send(`arrived ${process.env.STATUS} ${req.params.id}\n`);
 });
 
 app.post("/newStudent", (req, res) => {
